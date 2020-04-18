358ae57f394ff7fef9c63a75392a6b572eadefe2
takeyukigoto <takeoamiki@hotmail.co.jp>
2020-04-18 15:40:28 +0900

    gazo check

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,12 +5,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-
+  process resize_to_fit: [800, 800]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    process resize_to_fit: [800, 800]
+
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.13.0'
 
 # Capistranoのログの表示に利用する
-set :application, '<chat-space>'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:takeyukigoto/chat-space.git'
